727c5fd1c132cd10de3caeb81596aa312c911724
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2022-03-24 14:30:56 -0300

    Removing default value

--- scripts.js
+++ scripts.js
@@ -1,17 +1,7 @@
 const breakLine = "<br/>";
-const defaultValue = `
-// "username" serve como um armazenador de dados, e nesse caso vai registrar um nome
-const username = prompt("Ola! Qual e o seu nome?");
-
-// chamamos esse armazenador de variavel
-// verificamos se a variavel possui um dado valido
-if (username !== null)
-  alert("Seja bem vindo, " + username); // se o teste for verdadeiro, exibimos uma mensagem
-`.trim();
-
 const logStack = [];
+
 const editor = ace.edit("editor");
-editor.setValue(defaultValue, -1);
 editor.setTheme("ace/theme/monokai");
 editor.session.setOptions({ tabSize: 2, useSoftTabs: true });
 editor.session.setMode("ace/mode/javascript");
